5cbc67c6720288f5b4d6f868425ef2834e49c6e3
Ketan Kumavat <ketan.kumavat1984@gmail.com>
2023-12-08 15:06:12 +0530

    Commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
+import React, { useState,useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from "./appWrite/auth";
+import {login, logout } from "./store/authSlice"
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>Ketan</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispaytch(login({userData}))
+      }else {
+        dispatch(logout())
+      }
+    } )
+    .finally( () => setLoading(false))
+  }, []);
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-300'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 
--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -20,6 +20,6 @@ const authSlice = createSlice({
     }
 })
 
-export const {} = authSlice.actions;
+export const {login, logout} = authSlice.actions;
 
 export default authSlice.reducer;
